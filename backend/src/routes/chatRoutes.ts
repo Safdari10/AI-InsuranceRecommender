7bda17028d289be4668b41550bc8cbf7d3f7c037
Zaker Safdari <zakersafdari@gmail.com>
2024-12-06 22:41:44 +1300

    create get route for starting chat

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getChat } from '../controllers/chatController';
-
+import { getChat, startChat } from '../controllers/chatController';
 
 const router = Router();
 
+router.get('/start-chat', startChat);
 router.post('/chat', getChat);
 
 export default router;
