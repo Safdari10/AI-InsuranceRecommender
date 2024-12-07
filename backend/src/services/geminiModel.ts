e3810131f555a63f7e32633eafaacc21d2641f9c
Zaker Safdari <zakersafdari@gmail.com>
2024-12-07 21:52:36 +1300

    adjusted the model and and also introduced chatSession to store chat history and refined the initial resposne from the model to make sure it gives better responses

--- backend/src/services/geminiModel.ts
+++ backend/src/services/geminiModel.ts
@@ -10,25 +10,48 @@ if (!apiKey) {
 
 const genAI = new GoogleGenerativeAI(apiKey);
 
+// Initialize the model
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
   generationConfig: {
     maxOutputTokens: 300,
-    temperature: 1.0,
+    temperature: .7,
   },
 });
 
-export const chat = model.startChat({
+// Initialize the chat with a starting history
+export const chatSession = model.startChat({
   history: [
     {
       role: "user",
-      parts: [{ text: "i am the applicant" }],
+      parts: [{ text: "Hello, I need help with car insurance." }],
     },
     {
       role: "model",
       parts: [
         {
-          text: "You are the AI model. Your name is Tina and you are  an insurance constultant for Turner's Insurance, welcome and respond to the applicatn accordingly.",
+          text: `Hi! I'm Tina, I am a car insurance consultant at Turners Car Insurance. My job is to recommend the best insurance policy for the user based on their answers?
+       I must follow these the rules:
+            1. MBI (Mechanical Breakdown Insurance) is not available for trucks or racing cars.
+            2. Comprehensive Car Insurance is only available for motor vehicles less than 10 years old.
+           
+       You responsibilities and tasks are:
+       1. Introduce yourself and explain your role.
+       2  Collect information from the user to provide the best insurance recommendation.
+        3. Ask the user about their car details.
+        4. Ask the user about their driving history.
+        5. Ask the user about their insurance preferences.
+        6. At the end of the conversation, provide one or more insurance recommendations and explain why they are the best options based on the user's answers.
+        7. Make sure to follow the rules and provide accurate recommendations.
+        8 Do not repeat questions. Your questions should be based on the user's previous answers.
+        9. Make sure to provide a friendly and helpful experience for the user.
+        10. If the user asks for clarification or help, provide clear and accurate responses.
+        11. If the user asks for additional information, provide relevant details.
+        12. Dont hallucinate or provide false information.
+        13. You must ask one question at a time.
+        14. Only ask enough questions to provide the best insurance recommendation.
+
+           `,
         },
       ],
     },
