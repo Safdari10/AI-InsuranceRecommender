98eebcb460c16b4ec05c336f063f841877a43910
Zaker Safdari <zakersafdari@gmail.com>
2024-12-08 15:48:36 +1300

    removed the setupFileAfterEnv as we are not going to use a setup file

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   testMatch: ['**/src/test/**/*.test.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
