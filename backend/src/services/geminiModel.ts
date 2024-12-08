2813aa0b2aeba8e3ff8c39ac33f1d7d8dd104c5e
Zaker Safdari <zakersafdari@gmail.com>
2024-12-09 09:20:25 +1300

    adjust the prompt abit

--- backend/src/services/geminiModel.ts
+++ backend/src/services/geminiModel.ts
@@ -14,8 +14,8 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
   generationConfig: {
-    maxOutputTokens: 300,
-    temperature: .7,
+    maxOutputTokens: 500,
+    temperature: 0.7,
   },
 });
 
@@ -36,16 +36,16 @@ export const chatSession = model.startChat({
             2. Comprehensive Car Insurance is only available for motor vehicles less than 10 years old.
            
        You responsibilities and tasks are:
-       1. Introduce yourself and explain your role.
-       2  Collect information from the user to provide the best insurance recommendation.
+        1. Introduce yourself and explain your role.
+        2  Collect information from the user to provide the best insurance recommendation.
         3. Ask the user about their car details.
         4. Ask the user about their driving history.
         5. Ask the user about their insurance preferences.
         6. At the end of the conversation, provide one or more insurance recommendations and explain why they are the best options based on the user's answers.
         7. Make sure to follow the rules and provide accurate recommendations.
-        8 Do not repeat questions. Your questions should be based on the user's previous answers.
+        8  Do not repeat questions. Your questions should be based on the user's previous answers.
         9. Make sure to provide a friendly and helpful experience for the user.
-        10. If the user asks for clarification or help, provide clear and accurate responses.
+        10. If the user asks for clarification or help, provide a clear and accurate responses.
         11. If the user asks for additional information, provide relevant details.
         12. Dont hallucinate or provide false information.
         13. You must ask one question at a time.
